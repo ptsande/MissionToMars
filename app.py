e867d696909bc6934d266553388dd2a0eb0f0462
Patrick Sande <patricktsande@gmail.com>
2018-11-16 20:59:20 -0600

    updated files for homework, still incomplete

--- app.py
+++ app.py
@@ -8,26 +8,16 @@ app = Flask(__name__)
 # Use PyMongo to establish Mongo connection
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
-# Route to render index.html template using data from Mongo
 @app.route("/")
-def home():
+def index():
     mars_data = mongo.db.collection.find_one()
-    return render_template('index.html', marsDash=mars_data)
+    return render_template("index.html", mars_data=mars_data)
 
-
-# Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
-
-    # Run the scrape function
-    scrape_mars = scrape_mars.scrape_info()
-
-    # Update the Mongo database using update and upsert=True
+    scrape_mars = scrape_mars.scrape()
     mongo.db.collection.update({}, mars_data, upsert=True)
-
-    # Redirect back to home page
     return redirect("/")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -9,22 +9,17 @@ def init_browser():
     return Browser("chrome", **executable_path, headless=False)
 
 # Scrape Recent NASA Mars News
-def scrape_news():
+def scrape():
     browser = init_browser
-    url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
-    browser.visit(url)
+    news_url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+    browser.visit(news_url)
+    time.sleep(2)
     html = browser.html
     soup = bsoup(html, "lxml")
-    news_results = soup.find_all("li", class_="slide")
-    for result in news_results:
-        news_title = result.find("div", class_="content_title").text
-        news_p = result.find("div", class_="article_teaser_body").text
-        mars_headlines = {
-            'Headline': news_title,
-            'Description': news_p
-        }
-    browser.quit()
-    return mars_headlines
+    news_title = soup.find("div", class_="content_title").text
+    news_p = soup.find("div", class_="article_teaser_body").text
+    mars_news['news_title'] = news_title
+    mars_news['news_paragraph'] = news_p
 
 # JPL Mars Space Images - Featured Image
 # image_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
@@ -39,26 +34,31 @@ def scrape_news():
 # featured_image_url
 
 # Mars Weather
-def scrape_weather():
-    browser = init_browser
     weather_url = "https://twitter.com/marswxreport?lang=en"
     browser.visit(weather_url)
-    html = browser.html
-    soup = bsoup(html, "lxml")
+    time.sleep(2)
+    weather_html = browser.html
+    soup = bsoup(weather_html, "lxml")
     mars_weather = soup.find("div", class_="js-tweet-text-container").text
-    browser.quit()
-    return mars_weather
 
 # Mars Facts
-def scrape_facts():
-    browser = init_browser
     facts_url = "https://space-facts.com/mars/"
+    browser.visit(facts_url)
+    time.sleep(2)
     tables = pd.read_html(facts_url)
     mars_df = tables[0]
     mars_df.columns = ['Observation', 'Measurement']
     mars_facts = mars_df.set_index('Observation', inplace=True)
+
+    mars_data = {
+    "mars_news": mars_news,
+    "mars_weather": mars_weather,
+    "mars_facts": mars_facts
+    }
     browser.quit()
-    return mars_facts
+
+    return mars_data
+
 
 # Mars Hemispheres
 #hemi_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
